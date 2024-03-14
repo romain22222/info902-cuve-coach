d11980f780ed1897eb32935c8a93aa3fc6f79f04
romain22222 <romrom22222@hotmail.fr>
2024-03-14 13:42:50 +0100

    hopefully the last commit

--- database.py
+++ database.py
@@ -6,8 +6,8 @@ import mysql.connector
 
 conn = mysql.connector.connect(
 	host="localhost",
-	user="azertyuiop",
-	password="123456789",
+	user="root",
+	password="azertyuiop@123456789",
 	database="mysql"
 )
 
