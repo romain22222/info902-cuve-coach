5499d9a77f73f5a3b8040da71d6db06d522577f7
romain22222 <romrom22222@hotmail.fr>
2024-03-14 14:51:25 +0100

    hopefully the last commit

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def coachTime(pm: database.PlantManagment, timeChoice: int, coachRepeat: bool = 
 
 
 def keyConnected() -> bool:
-	ret = subprocess.check_output('sudo mount /dev/sda1 /mnt')
+	ret = subprocess.check_output('sudo mount /dev/sda1 /mnt', shell=True)
 	os.system('sudo umount /mnt')
 	print("RESULT :"+ret)
 	return "does not exist" not in ret
